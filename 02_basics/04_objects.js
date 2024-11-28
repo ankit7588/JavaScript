995562193899d06008e7ce814e39497a0616bec2
ankit7588 <53566784+ankit7588@users.noreply.github.com>
2024-11-28 15:42:19 +0000

    objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -26,11 +26,55 @@ const obj3 = {5: "e", 6: "f"}
 // const obj4 = Object.assign({}, obj1, obj2, obj3)
 
 const obj4 = {...obj1, ...obj2, ...obj3}
-console.log(obj4);
+// console.log(obj4);
 
 
 const users = [
     {
-        
-    }
+        id: 1,
+        email: "a@gmail.com"
+    },
+    {
+        id: 2,
+        email: "n@gmail.com"
+    },
+    {
+        id: 3,
+        email: "k@gmail.com"
+    },
+]
+
+users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+const course = {
+    coursename: "javascript in hindi",
+    price: "999",
+    courseInstructor: "ankit"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+
+// {
+//     "name": "Ankit",
+//     "coursename": "javascript in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
 ]
