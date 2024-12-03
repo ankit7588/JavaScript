41d02218414ae6c6aee262025ca07cf46cf919aa
ankit7588 <53566784+ankit7588@users.noreply.github.com>
2024-12-03 10:18:03 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,12 +3,47 @@ let a = 300
 if (true) {
     let a = 10
     const b = 20
-    console.log("INNER: ", a);
+    // console.log("INNER: ", a);
     
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
 
 
+function one() {
+    const username = "ankit"
+
+    function two() {
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    
+    two()   
+}
+
+// one()
+
+if (true) {
+    const username = "ankit"
+    if (username === "ankit") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);   
+}
+// console.log(username);
+
+//  +++++++++++++++++++++++ interesting +++++++++++++++++++++
+
+console.log(addOne(6))
+function addOne(num) {
+    return num + 1
+}
+
+addTwo()
+const addTwo = function(num) {
+    return num + 2
+}
